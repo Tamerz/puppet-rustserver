ba428475132cfe1c6dffd58e829cfcfea3facb12
Tames McTigue <tames@northwestern.edu>
2021-09-21 14:31:36 +0300

    Working on start script

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,11 +6,37 @@
 #   rustserver::server { 'namevar': }
 define rustserver::server (
   String $install_dir,
+  String $rcon_password,
   Enum['present', 'absent'] $ensure = present,
+  String $ip_address = '0.0.0.0',
+  String $rcon_ip_address = '0.0.0.0',
+  Integer $port = 28015,
+  Integer $rcon_port = 28016,
+  Integer $max_players = 10,
 ) {
 
   exec { "install ${title} server":
     command => "${rustserver::steamcmd_path} +login +force_install_dir ${install_dir} +app_update 258550 +quit",
     creates => $install_dir,
+  }
+
+  file { "${title} start script":
+    ensure  => file,
+    path    => "${install_dir}/start.sh",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => epp(
+      'rustserver/start.sh.epp',
+      {
+        'install_dir'     => $install_dir,
+        'ip_address'      => $ip_address,
+        'port'            => $port,
+        'rcon_ip_address' => $rcon_ip_address,
+        'rcon_port'       => $rcon_port,
+        'rcon_password'   => $rcon_password,
+        'max_players'     => $max_players,
+      }
+    ),
   }
 }
--- spec/defines/server_spec.rb
+++ spec/defines/server_spec.rb
@@ -8,6 +8,7 @@ describe 'rustserver::server' do
   let(:params) do
     {
       install_dir: '/opt/rustservers/vanilla-01',
+      rcon_password: 's0s3cret',
     }
   end
 
@@ -22,6 +23,23 @@ describe 'rustserver::server' do
           'command' => '/opt/steamcmd/steamcmd.sh +login +force_install_dir /opt/rustservers/vanilla-01 +app_update 258550 +quit',
           'creates' => '/opt/rustservers/vanilla-01',
         )
+      }
+
+      it {
+        is_expected.to contain_file('vanilla-01 start script').with(
+          'ensure' => 'file',
+          'path'   => '/opt/rustservers/vanilla-01/start.sh',
+          'owner'  => 'root',
+          'mode'   => '0755',
+        )
+        .with_content(%r{^/opt/rustservers/vanilla-01/RustDedicated -batchmode -nographics \\$})
+        .with_content(%r{^  -server.ip 0.0.0.0 \\$})
+        .with_content(%r{^  -server.port 28015 \\$})
+        .with_content(%r{^  -rcon.ip 0.0.0.0 \\$})
+        .with_content(%r{^  -rcon.port 28016 \\$})
+        .with_content(%r{^  -rcon.password "s0s3cret" \\$})
+        .with_content(%r{^  -server.maxplayers 10 \\$})
+        #.that_contains(%r{'-server.ip 0.0.0.0}-server.port 28015 -rcon.ip 0.0.0.0 -rcon.port 28016 -rcon.password "s0s3cret" -server.maxplayers 10 -server.hostname "My Rust Server" ')
       }
     end
   end
